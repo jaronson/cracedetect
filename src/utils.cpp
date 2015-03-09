1fcb8dc4150790f04fcde403555cb804ef908b17
Josh Aronson <jparonson@gmail.com>
2015-03-09 10:05:35 -0500

    utils.cpp lint

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,17 +1,18 @@
 #include "./utils.h"
-
-using namespace std;
+#include <string>
+#include <vector>
 
 namespace utils {
-  vector<string> glob(const string& pattern){
-    using namespace std;
+  using std::string;
+  using std::vector;
 
+  vector<string> glob(const string& pattern) {
     glob_t glob_result;
     vector<string> ret;
 
     glob(pattern.c_str(), GLOB_TILDE, NULL, &glob_result);
 
-    for(unsigned int i=0; i < glob_result.gl_pathc; ++i){
+    for (unsigned int i = 0; i < glob_result.gl_pathc; ++i) {
       ret.push_back(string(glob_result.gl_pathv[i]));
     }
 
@@ -20,8 +21,8 @@ namespace utils {
     return ret;
   }
 
-  void normalizeImage(Mat source, Mat target){
+  void normalizeImage(Mat source, Mat target) {
     cvtColor(source, target, CV_BGR2GRAY);
     equalizeHist(target, target);
   }
-}
+}  // namespace utils
